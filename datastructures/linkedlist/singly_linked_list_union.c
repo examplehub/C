5bc549d4c84d7dad1f15a6e35ad9cbf3827fd215
shellhub <shellhub.me@gmail.com>
2020-11-26 15:54:37 +0800

    complete singly linked list

--- datastructures/linkedlist/singly_linked_list_union.c
+++ datastructures/linkedlist/singly_linked_list_union.c
@@ -32,12 +32,45 @@ void insertOrder(Node *root, ElemType elem) {
     root->length++;
 }
 
+void insertNth(Node *root, int index, ElemType elem) {
+    if (index < 0 || index > root->length) {
+        perror("invalid insertion index");
+    }
+    Node *temp = root;
+    for (int i = 0; i < index; ++i) {
+        temp = temp->next;
+    }
+
+    Node *newNode = malloc(sizeof(Node));
+    newNode->data = elem;
+    newNode->next = temp->next;
+    temp->next = newNode;
+    root->length++;
+}
+
+ElemType deleteNth(Node *root, int index) {
+    if (index < 0 || index > root->length - 1) {
+        perror("invalid delete index");
+    }
+    Node *temp = root;
+    for (int i = 0; i < index; ++i) {
+        temp = temp->next;
+    }
+    Node *destroy = temp->next;
+    ElemType data = destroy->data;
+    temp->next = temp->next->next;
+    free(destroy);
+    root->length--;
+    return data;
+}
+
 void printSinglyLinkedList(Node *root) {
     Node *temp = root->next;
     for (int i = 0; i < root->length; ++i) {
         printf("%d\t", temp->data);
         temp = temp->next;
     }
+    printf("\n");
 }
 
 void test() {
@@ -48,7 +81,21 @@ void test() {
     for (int i = 0; i <= 4; i += 2) {
         insertOrder(root, i);
     }
+    printSinglyLinkedList(root); /* output: 0	1	2	3	4	5 */
+
+    root = init(); /* init new linked list */
+    for (int i = 0; i <= 3; ++i) {
+        insertNth(root, i, i + 1);
+    }
+    insertNth(root, 0, 0);
+    insertNth(root, 5, 666);
+    printSinglyLinkedList(root); /* output: 0	1	2	3	4	666 */
+
+    assert(deleteNth(root, 0) == 0);
+    assert(deleteNth(root, 4) == 666);
     printSinglyLinkedList(root);
+    assert(deleteNth(root, 2) == 3);
+    printSinglyLinkedList(root); /* output: 1	2	4 */
 }
 
 int main() {
