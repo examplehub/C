fb5ba0c885f38bf8704217dd575514fa99639ba5
shellhub <shellhub.me@gmail.com>
2020-11-27 09:09:22 +0800

    add linked_stack

---
+++ datastructures/stack/linked_stack.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdlib.h>
+
+typedef int ElemType;
+typedef struct Node {
+    ElemType data;
+    struct Node *next;
+} Node;
+
+typedef struct LinkedStack {
+    Node *top; /* the top of this stack */
+    int size; /* the number of elements in stack */
+} LinkedStack;
+
+/**
+ * Initialized stack.
+ * @param pStack
+ */
+void initStack(LinkedStack *pStack) {
+    pStack->top = NULL;
+    pStack->size = 0;
+}
+
+/**
+ * Test if the stack is empty or not.
+ * @param stack the stack to be test.
+ * @return true if stack is empty, otherwise false.
+ */
+bool isEmpty(LinkedStack stack) {
+    return stack.size == 0;
+}
+
+/**
+ * Return the number of elements at the stack.
+ * @param stack the stack contains elements.
+ * @return the number of elements at the stack.
+ */
+bool size(LinkedStack stack) {
+    return stack.size;
+}
+
+/**
+ * Add element to top of stack.
+ * @param pStack the pointer of stack.
+ * @param elem the element to be added.
+ * @return true if added successfully.
+ */
+bool push(LinkedStack *pStack, ElemType elem) {
+    Node *newNode = malloc(sizeof(Node));
+    if (newNode == NULL) {
+        perror("stackoverflow");
+    }
+    newNode->data = elem;
+    newNode->next = pStack->top;
+    pStack->top = newNode;
+    pStack->size++;
+    return true;
+}
+
+/**
+ * Remove element at top of the stack.
+ * @param pStack the pointer of stack.
+ * @return element at the top of stack.
+ */
+ElemType pop(LinkedStack *pStack) {
+    if (pStack->size == 0) {
+        perror("Can't pop from empty stack");
+    }
+    Node *poppedNode = pStack->top;
+    ElemType poppedElem = poppedNode->data;
+    pStack->top = pStack->top->next;
+    free(poppedNode);
+    pStack->size--;
+    return poppedElem;
+}
+
+/**
+ * Print stack elements.
+ * @param stack the stack to be printed.
+ */
+void printStack(LinkedStack stack) {
+    Node *temp = stack.top;
+    while (temp != NULL) {
+        printf("%d\t", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void test() {
+    LinkedStack linkedStack;
+    initStack(&linkedStack);
+    assert(isEmpty(linkedStack));
+    assert(size(linkedStack) == 0);
+
+    for (int i = 1; i <= 5; ++i) {
+        push(&linkedStack, i);
+    }
+    printStack(linkedStack); /* output: 5	4	3	2	1 */
+
+    for (int i = 5; i >= 1; --i) {
+        assert(pop(&linkedStack) == i);
+    }
+
+    assert(size(linkedStack) == 0);
+    assert(isEmpty(linkedStack));
+    printStack(linkedStack); /* output: */
+}
+
+int main() {
+    test();
+    return 0;
+}
