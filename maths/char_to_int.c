cadb7d5b550637b614ffe88b73b38eeebc290aeb
shellhub <shellhub.me@gmail.com>
2020-12-13 13:50:20 +0800

    convert character to int

---
+++ maths/char_to_int.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/**
+ * Convert digit character to integer.
+ * @param ch character to be converted.
+ * @return integer number.
+ */
+int toInt(char ch) {
+    return ch - '0';
+}
+
+void test() {
+    for (char ch = '0', i = 0; ch <= '9'; ch++, i++) {
+        assert(toInt(ch) == i);
+    }
+}
+
+int main() {
+    return 0;
+}
+
