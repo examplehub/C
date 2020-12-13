476deac661533c357ae9520ef49ed293bad9991e
Du Yuanchao <shellhub.me@gmail.com>
2020-12-13 18:50:20 +0800

    Update maths (#22)
    * convert character to int
    
    * count digits
    
    * remove unused header file
    
    * count digit recursion

---
+++ maths/char_to_int.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+
+/**
+ * Convert digit character to integer.
+ * @param ch character to be converted.
+ * @return integer number.
+ */
+int toInt(char ch) {
+    return ch - '0';
+}
+
+void test() {
+    for (char ch = '0', i = 0; ch <= '9'; ch++, i++) {
+        assert(toInt(ch) == i);
+    }
+}
+
+int main() {
+    test();
+    return 0;
+}
+
