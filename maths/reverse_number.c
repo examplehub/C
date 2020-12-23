60f651bb6190908f38e8273c8e491732b0454668
Du Yuanchao <shellhub.me@gmail.com>
2020-12-23 10:28:07 +0800

    Update math library (#30)
    * pow recursion
    
    * reverse number
    
    * sign function

---
+++ maths/reverse_number.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+#include <stdbool.h>
+
+int reverse(int number) {
+    int reversed = 0;
+    while (number != 0) {
+        reversed = reversed * 10 + number % 10;
+        number /= 10;
+    }
+    return reversed;
+}
+
+void test() {
+    assert(-123 % 10 == -3);
+    assert(123 % -10 == 3);
+    assert(-123 % -10 == -3);
+    assert(21 == reverse(12));
+    assert(-21 == reverse(-12));
+    assert(123456 == reverse(654321));
+    assert(-123456 == reverse(-654321));
+    assert(0 == reverse(0));
+}
+
+int main() {
+    test();
+    return 0;
+}
