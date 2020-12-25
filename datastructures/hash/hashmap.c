69e6c817d76e65565cf34e9af9bd2234332f6895
Du Yuanchao <shellhub.me@gmail.com>
2020-12-25 13:43:01 +0800

    fixed hashmap bug (#36)

--- datastructures/hash/hashmap.c
+++ datastructures/hash/hashmap.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdbool.h>
 
 #define MAX_SIZE 20
 
@@ -27,13 +28,34 @@ void init(HashMap *hashMap) {
 
 void put(HashMap *hashMap, int key, int value) {
     int hash = hashCode(key);
-    while (hashMap->array[hash] != NULL) {
+    while (hashMap->array[hash] != NULL && hashMap->array[hash]->key != key) {
         hash = (hash + 1) % MAX_SIZE;
     }
-    Item *newItem = malloc(sizeof(Item));
-    newItem->key = key;
-    newItem->value = value;
-    hashMap->array[hash] = newItem;
+    bool exists = hashMap->array[hash] != NULL && hashMap->array[hash]->key == key;
+    if (exists) {
+        hashMap->array[hash]->value = value;
+    }else{
+        Item *newItem = malloc(sizeof(Item));
+        newItem->key = key;
+        newItem->value = value;
+        hashMap->array[hash] = newItem;
+        hashMap->size++;
+    }
+}
+
+int delete(HashMap *hashMap, int key) {
+    int hash = hashCode(key);
+    while (hashMap->array[hash] != NULL) {
+        if (hashMap->array[hash]->key == key) {
+            int value = hashMap->array[hash]->value;
+            free(hashMap->array[hash]);
+            hashMap->array[hash] = NULL;
+            hashMap->size--;
+            return value;
+        }
+        hash = (hash + 1) % MAX_SIZE;
+    }
+    return -1;
 }
 
 int get(HashMap *hashMap, int key) {
@@ -45,6 +67,10 @@ int get(HashMap *hashMap, int key) {
         hash = (hash + 1) % MAX_SIZE;
     }
     return -1;
+}
+
+int size(HashMap *hashMap) {
+    return hashMap->size;
 }
 
 void printHashMap(HashMap *hashMap) {
@@ -75,11 +101,21 @@ void test() {
     HashMap hashMap;
     init(&hashMap);
     for (int i = 0; i < len; ++i) {
+        assert(hashMap.size == i);
         put(&hashMap, data[i][0], data[i][1]);
     }
     for (int i = 0; i < len; i++) {
         assert(get(&hashMap, data[i][0]) == data[i][1]);
     }
+
+    assert(size(&hashMap) == 9);
+    put(&hashMap, 37, 666); /* update value */
+    assert(get(&hashMap, 37) == 666);
+    assert(get(&hashMap, 111) == -1);
+    assert(size(&hashMap) == 9);
+    assert(delete(&hashMap, 4) == 25);
+    assert(delete(&hashMap, 777) == -1);
+    assert(size(&hashMap) == 8);
 }
 
 int main() {
