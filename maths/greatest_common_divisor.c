f10a0373bec2395a158fe98674867548c794a38c
Du Yuanchao <shellhub.me@gmail.com>
2020-12-29 08:21:57 +0800

    greatest_common_divisor (#39)

---
+++ maths/greatest_common_divisor.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+
+/**
+ * Calculate the greatest common divisor of two numbers.
+ * @param a the first number.
+ * @param b the second number.
+ * @return the greatest common divisor of given two numbers.
+ */
+int gcd(int a, int b) {
+    if (a == 0 || b == 0) {
+        return 0;
+    }
+    int t = 1;
+    while (t != 0) {
+        t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
+void test() {
+    assert(gcd(10, 15) == 5);
+    assert(gcd(10, 0) == 0);
+    assert(gcd(0, 10) == 0);
+    assert(gcd(15, 17) == 1);
+}
+
+int main() {
+    test();
+    return 0;
+}
