65ed1e32d182cbd741ae80b1bb9ef6730631e499
Du Yuanchao <shellhub.me@gmail.com>
2021-03-22 20:27:41 +0800

    depth_of_binary_tree (#55)

---
+++ datastructures/binarytree/depth_of_binary_tree.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef int ElemType;
+typedef struct Node {
+    ElemType data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+/**
+ * Create a new node with given data.
+ * @param data the data store at node.
+ * @return new node.
+ */
+Node *createNode(int data) {
+    Node *newNode = (malloc(sizeof(Node)));
+    newNode->data = data;
+    return newNode;
+}
+
+int depth(Node *root) {
+    if (root == NULL) {
+        return 0;
+    } else {
+        int leftDepth = depth(root->left);
+        int rightDepth = depth(root->right);
+        return leftDepth > rightDepth ? leftDepth + 1: rightDepth + 1;
+    }
+}
+
+void test() {
+    /* see images/example_binary_tree.png */
+    Node *root = createNode(1);
+    Node *node2 = createNode(2);
+    Node *node3 = createNode(3);
+    Node *node4 = createNode(4);
+    Node *node5 = createNode(5);
+
+    root->left = node2;
+    root->right = node3;
+
+    node2->left = node4;
+    node2->right = node5;
+
+    node3->left = node3->right = NULL;
+    node4->left = node4->right = NULL;
+    node5->left = node5->right = NULL;
+
+    assert(depth(root) == 3);
+}
+
+int main() {
+    test();
+    return 0;
+}
