5c95e87f8988592cf9cea9cc002baa40d0de63b2
shellhub <shellhub.me@gmail.com>
2021-05-12 20:07:06 +0800

    hash table

---
+++ datastructures/hash/hash_table.c
@@ -0,0 +1,43 @@
+#include <assert.h>
+
+#define MAX 13
+
+/**
+ * Return hash address
+ * @param key the key value to be calculate.
+ * @return hash address of given key value.
+ */
+int hash(int key) {
+    return key % MAX;
+}
+
+/**
+ * Put key value to hash table.
+ * @param hashTable the array store elements.
+ * @param key the key to be put.
+ */
+void put(int hashTable[], int key) {
+    int index = hash(key);
+    while (hashTable[index] != 0) {
+        index = (index + 1) % MAX;
+    }
+    hashTable[index] = key;
+}
+
+void test() {
+    int arr[] = {25, 51, 8, 22, 26, 67, 11, 16, 54, 41};
+    int hashTable[MAX] = {0};
+    for (int i = 0, len = sizeof(arr) / sizeof(arr[0]); i < len; ++i) {
+        put(hashTable, arr[i]);
+    }
+    int data[] = {51, 26, 67, 16, 54, 41, 0, 0, 8, 22, 0, 11, 25};
+    for (int i = 0, len = sizeof(data) / sizeof(data[0]); i < len; ++i) {
+        assert(data[i] == hashTable[i]);
+    }
+}
+
+int main() {
+    test();
+    return 0;
+}
+
