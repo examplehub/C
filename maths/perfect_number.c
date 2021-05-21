0bcf3e85737927a23a849de182a8d54daa899f8e
shellhub <shellhub.me@gmail.com>
2021-05-21 19:20:33 +0800

    perfect number optimization

--- maths/perfect_number.c
+++ maths/perfect_number.c
@@ -11,7 +11,7 @@ bool isPerfectNumber(int number) {
         return false; /* all negative is not perfect number */
     }
     int sum = 0;
-    for (int i = 1; i < number; i++) {
+    for (int i = 1; i <= number / 2; i++) {
         if (number % i == 0) {
             sum += i;
         }
