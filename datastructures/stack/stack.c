31cd08514af36e7695685618844cc3e3cbcaa9f3
shellhub <shellhub.me@gmail.com>
2021-05-25 16:59:35 +0800

    convert structure pointer

--- datastructures/stack/stack.c
+++ datastructures/stack/stack.c
@@ -15,16 +15,16 @@ void initStack(Stack *pStack) {
     pStack->top = -1;
 }
 
-bool isEmpty(Stack stack) {
-    return stack.top == -1;
+bool isEmpty(Stack *pStack) {
+    return pStack->top == -1;
 }
 
-bool isFull(Stack stack){
-    return stack.top == MAX_SIZE - 1;
+bool isFull(Stack *pStack){
+    return pStack->top + 1 == MAX_SIZE;
 }
 
-int length(Stack stack) {
-    return stack.top + 1;
+int length(Stack *pStack) {
+    return pStack->top + 1;
 }
 
 void clear(Stack *pStack) {
@@ -37,14 +37,14 @@ void destroy(Stack *pStack) {
 }
 
 ElemType pop(Stack *pStack){
-    if (isEmpty(*pStack)){
+    if (isEmpty(pStack)){
         perror("pop from empty stack.");
     }
     return pStack->base[pStack->top--];
 }
 
 bool push(Stack *pStack, ElemType elem){
-    if (isFull(*pStack)){
+    if (isFull(pStack)){
         perror("can't push full stack.");
         return false;
     }
@@ -52,30 +52,30 @@ bool push(Stack *pStack, ElemType elem){
     return true;
 }
 
-ElemType peek(Stack stack) {
-    if (isEmpty(stack)) {
+ElemType peek(Stack *pStack) {
+    if (isEmpty(pStack)) {
         perror("can't peek from empty stack.");
     }
-    return stack.base[stack.top];
+    return pStack->base[pStack->top];
 }
 
 void test() {
     Stack stack;
     initStack(&stack);
-    assert(isEmpty(stack));
-    assert(length(stack) == 0);
+    assert(isEmpty(&stack));
+    assert(length(&stack) == 0);
 
     for (int i = 1; i <= 5; ++i) {
         push(&stack, i);
     }
-    assert(peek(stack) == 5);
-    assert(length(stack) == 5);
+    assert(peek(&stack) == 5);
+    assert(length(&stack) == 5);
     for (int i = 5; i >= 1; --i) {
         assert(pop(&stack) == i);
     }
 
     clear(&stack);
-    assert(isEmpty(stack));
+    assert(isEmpty(&stack));
 
     destroy(&stack);
 }
