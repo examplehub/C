6c393a3cc71e1c7b3f56d0dfcb5c57ef28a363b0
shellhub <shellhub.me@gmail.com>
2021-06-24 19:32:42 +0800

    update adjacency matrix

--- datastructures/graph/graph_adjacency_matrix.c
+++ datastructures/graph/graph_adjacency_matrix.c
@@ -1,18 +1,19 @@
 #include <assert.h>
+#include <stdio.h>
 #define VERTEX_NUM 5
-typedef char ElemType;
+typedef int ElemType;
 typedef struct {
     ElemType vertex[VERTEX_NUM];
     int edge[VERTEX_NUM][VERTEX_NUM];
 }Graph;
 
-void initVertex(Graph *graph) {
+void initGraph(Graph *graph) {
+    /* init vertex */
     for (int i = 0; i < VERTEX_NUM; ++i) {
-        graph->vertex[i] = '\0';
+        graph->vertex[i] = -1;
     }
-}
 
-void initEdge(Graph *graph)  {
+    /* init edge */
     for (int i = 0; i < VERTEX_NUM; ++i) {
         for (int j = 0; j < VERTEX_NUM; ++j) {
             graph->edge[i][j] = 0;
@@ -20,62 +21,76 @@ void initEdge(Graph *graph)  {
     }
 }
 
-void initGraph(Graph *graph) {
-    initVertex(graph);
-    initEdge(graph);
-}
-
-/**
- * Insert vertexes graph with example vertexes, see images/adjacency_matrix_graph.jpeg
- * @param graph the graph store vertexes.
- */
-void insertVertex(Graph *graph) {
+int getOutOfDegree(Graph *graph, ElemType vertex) {
+    int vertexIndex = -1;
     for (int i = 0; i < VERTEX_NUM; ++i) {
-        graph->vertex[i] = 'A' + i;
-    }
-}
-
-/**
- * Insert edges with example graph. See images/adjacency_matrix_graph.jpeg
- * @param graph graph the graph store edges.
- */
-void insertEdge(Graph *graph) {
-    int matrix[VERTEX_NUM][VERTEX_NUM] = {
-            {0, 1, 1, 1, 0},
-            {1, 0, 0, 1, 1},
-            {1, 0, 0, 1, 0},
-            {1, 1, 1, 1, 1},
-            {0, 1, 0, 1, 0}
-    };
-    for (int i = 0; i < VERTEX_NUM; ++i) {
-        for (int j = 0; j < VERTEX_NUM; ++j) {
-            graph->edge[i][j] = matrix[i][j];
+        if (vertex == graph->vertex[i]) {
+            vertexIndex = i;
+            break;
         }
     }
+    if (vertexIndex == -1) {
+        perror("vertex not found.");
+    }
+    int sum = 0;
+    for (int i = 0; i < VERTEX_NUM; ++i) {
+        sum = sum + graph->edge[vertexIndex][i];
+    }
+    return sum;
+}
+
+int getInOfDegree(Graph *graph, ElemType vertex) {
+    int vertexIndex = -1;
+    for (int i = 0; i < VERTEX_NUM; ++i) {
+        if (vertex == graph->vertex[i]) {
+            vertexIndex = i;
+            break;
+        }
+    }
+    if (vertexIndex == -1) {
+        perror("vertex not found.");
+    }
+    int sum = 0;
+    for (int i = 0; i < VERTEX_NUM; ++i) {
+        sum = sum + graph->edge[i][vertexIndex];
+    }
+    return sum;
 }
 
 void test() {
     Graph graph;
     initGraph(&graph);
-    insertVertex(&graph);
-    insertEdge(&graph);
 
+    /* init vertexes */
     for (int i = 0; i < VERTEX_NUM; ++i) {
-        assert(graph.vertex[i] == 'A' + i);
+        graph.vertex[i] = i;
     }
 
+    /* init edges: see images/adjacency_matrix.png */
     int matrix[VERTEX_NUM][VERTEX_NUM] = {
-            {0, 1, 1, 1, 0},
-            {1, 0, 0, 1, 1},
-            {1, 0, 0, 1, 0},
-            {1, 1, 1, 1, 1},
-            {0, 1, 0, 1, 0}
+            {0, 1, 1, 0, 0},
+            {0, 0, 1, 0, 1},
+            {0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0}
     };
     for (int i = 0; i < VERTEX_NUM; ++i) {
         for (int j = 0; j < VERTEX_NUM; ++j) {
-            assert(graph.edge[i][j] == matrix[i][j]);
+            graph.edge[i][j] = matrix[i][j];
         }
     }
+
+    assert(2 == getOutOfDegree(&graph, 0));
+    assert(1 == getOutOfDegree(&graph, 2));
+    assert(2 == getOutOfDegree(&graph, 1));
+    assert(1 == getOutOfDegree(&graph, 3));
+    assert(0 == getOutOfDegree(&graph, 4));
+
+    assert(0 == getInOfDegree(&graph, 0));
+    assert(1 == getInOfDegree(&graph, 1));
+    assert(2 == getInOfDegree(&graph, 2));
+    assert(1 == getInOfDegree(&graph, 3));
+    assert(2 == getInOfDegree(&graph, 4));
 }
 
 int main() {
