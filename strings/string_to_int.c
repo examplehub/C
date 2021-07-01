c64ee44c2cbba5ed20b2ed38b9a03df9b0f3d955
shellhub <shellhub.me@gmail.com>
2021-07-01 18:18:46 +0800

    check null pointer

--- strings/string_to_int.c
+++ strings/string_to_int.c
@@ -1,7 +1,11 @@
 #include <assert.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int stringToInt(const char *string) {
+    if (string == NULL) {
+        return 0;
+    }
     bool isNegative = *string == '-';
     int number = 0;
     for (int i = isNegative ? 1 : 0; string[i] != '\0'; ++i) {
@@ -15,7 +19,8 @@ void test() {
     assert(123 == stringToInt("123"));
     assert(-123 == stringToInt("-123"));
     assert(123 == stringToInt("0123"));
-    //assert(123 == stringToInt("    123    "));//FIXME:去除空格
+    assert(0 == stringToInt(NULL));
+    //assert(123 == stringToInt("    123    "));//FIXME:remove white-space
 }
 
 int main() {
